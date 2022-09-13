8f72740e553571d079fdaa4c1e6333cd3a09fbb3
HP <HP@192.168.0.105>
2022-09-13 10:39:07 +0530

    First commit by engineer1- addProductToCart

---
+++ src/test/java/practice/QSPCartTest.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class QSPCartTest {
+
+	@Test
+	public void addProductToCart() {
+		System.out.println("product is added");
+	}
+	
+	
+}
